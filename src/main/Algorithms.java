0420d631374f86a5cc3d3edae9dd73f9c40be03a
Fabrício Almeida <fabrycio30@gmail.com>
2022-05-29 05:39:11 -0300

    add algorithms

--- src/main/Algorithms.java
+++ src/main/Algorithms.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Algorithms{
+	//bubble sort
 	static int[] bubbleSort(int[] arr) {  
 		int n = arr.length;  
 		int temp = 0;  
@@ -19,5 +20,52 @@ public class Algorithms{
 		}
 		return arr;
 	}
+
+
+	
+	// quick Sort
+	static void swap(int[] arr, int i, int j){
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		}
+
+	static int partition(int[] arr, int low, int high){
+		int pivot = arr[high];
+
+		int i = (low - 1);
+		for(int j = low; j <= high - 1; j++){
+			if (arr[j] < pivot){
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i + 1, high);
+		return (i + 1);
+	}
+
+	static int[] quickSort(int[] arr, int low, int high){
+		if (low < high){
+			int pi = partition(arr, low, high);
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+		}
+		return arr;
+	}
+	
+	  // shellSort
+    static int[] shellSort(int arr[]){
+        int n = arr.length;
+        for (int gap = n/2; gap > 0; gap /= 2){
+            for (int i = gap; i < n; i += 1){
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+                arr[j] = temp;
+            }
+        }
+        return arr;
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,29 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int[] num = Inputs.intsRandom(10000);//teste com 10k 
+		int[] num = Inputs.intsRandom(10);//teste com 10k 
 		
+		//BubbleSort
+		System.out.print("Bubble: ");
 		int[] bubble = Algorithms.bubbleSort(num);
-		
 		for (int i = 0; i <bubble.length; i++) {
-			System.out.print(bubble[i]+", ");
+			System.out.print(bubble[i]+" ");
 		}
+		
+		//QuickSort
+		System.out.println();
+		System.out.print("Quick: ");
+		int[] quick  = Algorithms.quickSort(num, 0, num.length - 1);
+		for(int i = 0; i < quick.length; i++)
+			System.out.print(quick[i] + " ");
+		
+		
+		//ShellSort
+		System.out.println();
+		System.out.print("Shell: ");
+		int[] shell = Algorithms.shellSort(num);
+		for(int i = 0; i < shell.length; i++)
+			System.out.print(shell[i] + " ");
 
 	}
 
