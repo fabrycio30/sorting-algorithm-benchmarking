9dbfd17b2ac8a26f9032b353d4ca7b2ab6197555
Fabrício Almeida <fabrycio30@gmail.com>
2022-06-09 19:07:12 -0300

    versao 1.0.0.0.0

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,28 +21,7 @@ public class Main {
 		  System.out.println("Adicione o caminho completo do arquivo:");
 		  System.out.println("=======================================");
 		  while(sc.hasNext()){ file_path = sc.next();break; } //sc.close();
-		//Lendo grafo de arquivo
-		  //RAM bubble
-		  //100 - RAM: 2366976bytes (2MB)
-		  //1000 - RAM: 2826240bytes (2MB)
-		  //10000 - RAM: 2367024bytes (2MB)
-		  //100000 - RAM: 3143440bytes (2MB)
-		  
-		  //RAM quick
-		  //100 - RAM: 2367016bytes (2MB)
-		  //1000 - RAM: 2660680bytes (2MB)
-		  //10000 - RAM: 2826240bytes (2MB)
-		  //100000 - RAM: 2767032bytes (2MB)
-		  //1000000 - 
-		  
-		  //RAM shell
-		  //100 - RAM: 2826240bytes (2MB)
-		  //1000 - RAM: 2826240bytes (2MB)
-		  //10000 - RAM: 2660680bytes (2MB)
-		  //100000 - RAM: 2767032bytes (2MB)
-		  //1000000 - RAM: 7020544bytes (6MB)
-		  
-		  
+	
 		String dados = "";
 		///home/fabricioalmeida/eclipse-workspace/sorting-algorithm-benchmarking/in.txt
 		try{    
@@ -94,44 +73,53 @@ public class Main {
 			System.out.print("RAM :"+media_ram_final+"(bytes)	||	"+bestCase[1]+"(bytes)	||	"+worstCase[1]+"(bytes)	\n");
 			
 		}else if(algoritmo.compareTo("quick_sort")==0) {
-			//QuickSort
-			System.out.println("\n-------------------------------------------------------------------------------------------------------");
-			System.out.println("Quick");
-			long timeQSInicial = System.currentTimeMillis();//inicia o time
-			GetFormat quick  = Algorithms.quickSort(num_ramdom, num_ramdom[0], num_ramdom.length-1);
-			long tq = runtimeFinal(timeQSInicial);//finaliza, calcula e salva o time
-			
-			long ram = quick.getConsumoRAM();
-			long ramMB = ram/(1024L * 1024L);
-			System.out.println("RAM: "+ ram+ "bytes ("+ramMB+"MB)");
-			
-			//printa entradas pequenas, só pra  mostrar a saida ordenada, N grandes não tem necessidade
-			if(quick.getArrayOrdenado().length<101) {
-
-				for (int i = 0; i < quick.getArrayOrdenado().length; i++) {
-					System.out.print(quick.getArrayOrdenado()[i]+", ");
-				}
+			//rodando 5 vezes para o caso randomico
+			long ram=0;long times=0;
+			for (int i = 0; i < 5; i++) {
+				long[] temp = runningQuick(num_ramdom, name_file_out);
+				times = times + temp[0];
+				ram = ram+ temp[1];
 			}
-			saveToFile(name_file_out, algoritmo, quick.getArrayOrdenado().length, Casos.getCaso(), tq, quick.getConsumoRAM());
+			long media_ram_final = ram/5;//em bytes
+			long media_tep_exec = times/5;//em millisegundos dados o pequeno tempo pra tamanhos pequenos
+			
+			long[] bestCase = runningQuick(num_melhor_caso, name_file_out);//calcula uma vez pro melhor caso. Array ja ordenado
+			long[] worstCase = runningQuick(num_pior_caso, name_file_out);//calcula uma vez pro pior caso também
+			
+			System.out.println("=============================================================================");
+			System.out.println("		RESULTADOS QUICK SORT (N="+num_melhor_caso.length+")				");
+			System.out.println("=============================================================================");
+			//System.out.println("");
+			
+			System.out.print("Média caso random	||	Melhor Caso	||	Pior caso	\n");
+			System.out.print("-----------------------------------------------------------------------------\n");
+			System.out.print("time:"+media_tep_exec+"(ms)		||	"+bestCase[0]+"(ms)		||	"+worstCase[0]+"(ms)	\n");
+			System.out.print("RAM :"+media_ram_final+"(bytes)	||	"+bestCase[1]+"(bytes)	||	"+worstCase[1]+"(bytes)	\n");
 			
 		}else if(algoritmo.compareTo("shell_sort")==0) {
-			//ShellSort
-			System.out.println("\n--------------------------------------------------------------------------------------------------------");
-			System.out.println("Shell ");
-			long timeSSInicial = System.currentTimeMillis();//inicia o time
-			GetFormat shell = Algorithms.shellSort(num_ramdom);
-			long ts = runtimeFinal(timeSSInicial);//finaliza, calcula e salva o time
-			long ram = shell.getConsumoRAM();
-			long ramMB = ram/(1024L * 1024L);
-			System.out.println("RAM: "+ ram+ "bytes ("+ramMB+"MB)");
-			//printa entradas pequenas, só pra  mostrar a saida ordenada, N grandes não tem necessidade
-			if(shell.getArrayOrdenado().length<101) {
-				for (int i = 0; i < shell.getArrayOrdenado().length; i++) {
-					System.out.print(shell.getArrayOrdenado()[i]+", ");
-				}
-			} 
-			saveToFile(name_file_out, algoritmo, shell.getArrayOrdenado().length, Casos.getCaso(), ts, shell.getConsumoRAM());
-
+			//rodando 5 vezes para o caso randomico
+			long ram=0;long times=0;
+			for (int i = 0; i < 5; i++) {
+				long[] temp = runningShell(num_ramdom, name_file_out);
+				times = times + temp[0];
+				ram = ram+ temp[1];
+			}
+			long media_ram_final = ram/5;//em bytes
+			long media_tep_exec = times/5;//em millisegundos dados o pequeno tempo pra tamanhos pequenos
+			
+			long[] bestCase = runningShell(num_melhor_caso, name_file_out);//calcula uma vez pro melhor caso. Array ja ordenado
+			long[] worstCase = runningShell(num_pior_caso, name_file_out);//calcula uma vez pro pior caso também
+			
+			System.out.println("=============================================================================");
+			System.out.println("		RESULTADOS SHELL SORT (N="+num_melhor_caso.length+")				");
+			System.out.println("=============================================================================");
+			//System.out.println("");
+			
+			System.out.print("Média caso random	||	Melhor Caso	||	Pior caso	\n");
+			System.out.print("-----------------------------------------------------------------------------\n");
+			System.out.print("time:"+media_tep_exec+"(ms)		||	"+bestCase[0]+"(ms)		||	"+worstCase[0]+"(ms)	\n");
+			System.out.print("RAM :"+media_ram_final+"(bytes)	||	"+bestCase[1]+"(bytes)	||	"+worstCase[1]+"(bytes)	\n");
+			
 			
 		}
 		else {System.out.println("Algorítmo não encontrado. Tente novamente!");}
@@ -143,13 +131,6 @@ public class Main {
 		System.out.println("Tempo de execução: "+ t + " ms");
 		return t;
 	}
-
-	/*
-	 * public static long getMemoriaRam() { // Get the Java runtime Runtime runtime
-	 * = Runtime.getRuntime(); // Calculate the used memory long ram_bytes =
-	 * runtime.totalMemory() - runtime.freeMemory(); long MB = 1024L * 1024L; long
-	 * ram = ram_bytes/MB; return ram_bytes; }
-	 */
 	public static void saveToFile(String filename, String algoritmo, int size_n, String caso, long tempo_exec, long ram){
 		Date tempo_atual = new Date();
 		tempo_atual.toString();
@@ -196,4 +177,52 @@ public class Main {
 		return result;
 	}
 
+	public static long[] runningQuick(int[] vetor, String name_file_out) {
+		//QuickSort
+		System.out.print("\n-------------------------\n");
+		//System.out.println("Quick");
+		long timeQSInicial = System.currentTimeMillis();//inicia o time
+		GetFormat quick  = Algorithms.quickSort(vetor, vetor[0], vetor.length-1);
+		long tq = runtimeFinal(timeQSInicial);//finaliza, calcula e salva o time
+		
+		long ram = quick.getConsumoRAM();
+		long ramMB = ram/(1024L * 1024L);
+		System.out.println("RAM: "+ ram+ "bytes ("+ramMB+"MB)");
+		
+		//printa entradas pequenas, só pra  mostrar a saida ordenada, N grandes não tem necessidade
+		if(quick.getArrayOrdenado().length<101) {
+
+			for (int i = 0; i < quick.getArrayOrdenado().length; i++) {
+				System.out.print(quick.getArrayOrdenado()[i]+", ");
+			}
+		}
+		saveToFile(name_file_out, "quick_sort", quick.getArrayOrdenado().length, Casos.getCaso(), tq, quick.getConsumoRAM());
+		long[] result = new long[2];
+		result[0] = tq; result[1]= ram;
+		return result;
+	}
+	public static long[] runningShell(int[] vetor, String name_file_out) {
+		//ShellSort
+		System.out.println("\n--------------------------------------------------------------------------------------------------------");
+		System.out.println("Shell ");
+		long timeSSInicial = System.currentTimeMillis();//inicia o time
+		GetFormat shell = Algorithms.shellSort(vetor);
+		long ts = runtimeFinal(timeSSInicial);//finaliza, calcula e salva o time
+		long ram = shell.getConsumoRAM();
+		long ramMB = ram/(1024L * 1024L);
+		System.out.println("RAM: "+ ram+ "bytes ("+ramMB+"MB)");
+		//printa entradas pequenas, só pra  mostrar a saida ordenada, N grandes não tem necessidade
+		if(shell.getArrayOrdenado().length<101) {
+			for (int i = 0; i < shell.getArrayOrdenado().length; i++) {
+				System.out.print(shell.getArrayOrdenado()[i]+", ");
+			}
+		} 
+		saveToFile(name_file_out, "shell_sort", shell.getArrayOrdenado().length, Casos.getCaso(), ts, shell.getConsumoRAM());
+
+		
+		long[] result = new long[2];
+		result[0] = ts; result[1]= ram;
+		return result;
+		
+	}
 }
